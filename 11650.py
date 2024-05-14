a708422ad9c3ed6a319dcd5cf664d479b304d855
UnityTrabler <kdhmm0325@naver.com>
2024-05-14 23:11:09 +0900

    11650

--- 11650.py
+++ 11650.py
@@ -1,27 +1,8 @@
-
 N = int(input())
-x_arr = [[0] * 2 for i in range(N)]
+arr = [[0] * 2 for i in range(N)]
 
-# for i in range(N):
-#     x_arr[i][0], x_arr[i][1] =  map(int, input().split())
-#     print("x = ",x_arr[i][0], "y =",x_arr[i][1])
-#     print("i = ", i)
-x_arr[0][1] = 33
-y_arr[0][1] = 33
-
-# for i in range(N):   
-#     for j in range(i+1, N):        
-#         if cmp(x_arr[i], x_arr[j]) == 1:             
-#             x_arr[i], x_arr[j] = x_arr[j], x_arr[i]
-#             y_arr[i], y_arr[j] = y_arr[j], y_arr[i]            
-#         elif cmp(x_arr[i], x_arr[j]) == 0:
-#             if cmp(y_arr[i], y_arr[j]) == 1:
-#                 x_arr[i], x_arr[j] = x_arr[j], x_arr[i]
-#                 y_arr[i], y_arr[j] = y_arr[j], y_arr[i]
-#             else : continue
-#         else : continue
-print(x_arr)
-print(y_arr)
-
-# for i in range(N):
-#     print(x_arr[i][0], x_arr[i][1])
+for i in range(N):
+    arr[i][0], arr[i][1] =  map(int, input().split())    
+arr.sort()
+for i in range(N):
+    print(arr[i][0], arr[i][1])
